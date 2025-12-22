8a5f4306a7f969cdfe183cce6d915c36e4822b4b
Kauko <kauko@biosynth.link>
2025-12-22 14:09:39 -0800

    Attempt to fix expiry

--- src/db/expiry.ts
+++ src/db/expiry.ts
@@ -4,10 +4,10 @@ export async function assertExpiry() {
   // Generic expiry
   await db
     .deleteFrom("expressSession")
-    .where("expires", "<", new Date())
+    .where("expires", "<", new Date().toISOString() as any)
     .execute();
   await db
     .deleteFrom("atprotoStateStore")
-    .where("createdAt", "<", new Date(Date.now() - 1000 * 60 * 60 * 24))
+    .where("createdAt", "<", new Date(Date.now() - 1000 * 60 * 60 * 24).toISOString() as any)
     .execute();
 }
